8f24177f8a2e981471f2870fd6afee52f877f4f7
LMS Labs Deploy <deploy@lms-labs.com>
2026-08-13 01:50:37 +0000

    release: local_sitefont v1.0.19

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,35 +1,6 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Upgrade steps for local_sitefont.
- *
- * @package    local_sitefont
- * @copyright  2026 LMS-Labs
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
- */
-
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * Execute local_sitefont upgrade from the given old version.
- *
- * @param int $oldversion
- * @return bool
- */
 function xmldb_local_sitefont_upgrade($oldversion) {
     if ($oldversion < 2026072300) {
         upgrade_plugin_savepoint(true, 2026072300, 'local', 'sitefont');
